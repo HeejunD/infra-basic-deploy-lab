91ab08276095fe2951414ee3cba381053328f4ff
HeejunD <snowd0919@gmail.com>
2026-03-23 19:18:46 +0900

    feat: 기능추가

--- infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
@@ -7,9 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class BackendController {
-
+	
     @GetMapping("/backend")
     public String backend() {
         return "backend";
+    }
+    
+    @GetMapping("/backend2")
+    public String backend2() {
+        return "backend2";
+    }
+    
+    @GetMapping("/backend3")
+    public String backend3() {
+        return "backend3";
+    }
+    
+    @GetMapping("/backend4")
+    public String backend4() {
+        return "backend4";
     }
 }
